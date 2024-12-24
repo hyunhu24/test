7af918e05e55eb882a6f95d4dff11d88e4bfa3d4
HyunHu <hyunhu024@gmail.com>
2024-12-24 19:15:00 +0900

    git study commit!!

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from './Component/Hello';
+
+function App() {
+  return (
+    <>
+    <Hello name = {"mike"}/>
+    <Hello name = {"john"}/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Component/Hello.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Hello = ({name}) => {
+    return(
+        <>
+        <h1>{name}</h1>
+        <p>name : {name}</p>
+        </>
+    )
+}
+
+export default Hello;
